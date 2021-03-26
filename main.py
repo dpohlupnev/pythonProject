e5bb3548ecf04ed36f852d8f70b21a92d1e0f221
dpohlupnev <dpohlupnev@gmail.com>
2021-03-26 11:32:04 +0300

    Add grades

--- main.py
+++ main.py
@@ -21,8 +21,11 @@ class Student:
 
     def grade_student(self):
         sum_grade = []
-        for grade in self.grades:
-            
+        for grade in self.grades.items():
+            sum_grade += grade[1]
+        self.average_rating_stud = str(round(sum(sum_grade) / len(sum_grade)))
+
+        return self.average_rating_stud
 
     def __str__(self):
         return f"Студент\nИмя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за домашние задания: \nКурсы в процессе изучения: {self.courses_in_progress}\nЗавершенные курсы: {self.finished_courses}\n"
@@ -42,10 +45,17 @@ class Mentor:
 class Lecturer(Mentor):
     def __init__(self, name, surname):
         self.student_grade = {}
+        self.average_rating_stud = None
         super().__init__(name, surname)
 
     def __str__(self):
-        return f"Лектор\nИмя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: \n"
+        return f"Лектор\nИмя: {self.name}\nФамилия: {self.surname}\nСредняя оценка за лекции: {self.average_rating_stud}\n"
+
+    def grade_student(self):
+        sum_grade = []
+        for grade in self.grades.items():
+            sum_grade += grade[1]
+        self.average_rating_stud = str(round(sum(sum_grade) / len(sum_grade)))
 
 
 class Reviewer(Mentor):
@@ -54,12 +64,20 @@ class Reviewer(Mentor):
 
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
-            if course in student.grades:
+            if course in student.grades.keys():
                 student.grades[course] += [grade]
             else:
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
+
+    # def grade_reviewer(self):
+    #     sum_grade = []
+    #     for grade in self.grades.items():
+    #         sum_grade += grade[1]
+    #     self.average_rating_stud = str(round(sum(sum_grade) / len(sum_grade)))
+    #
+    #     return self.average_rating_stud
 
     def __str__(self):
         return f"Проверяющий\nИмя: {self.name}\nФамилия: {self.surname}\n"
@@ -70,7 +88,7 @@ one_student.courses_in_progress += ['Python', 'Git']
 one_student.finished_courses += ['Java', 'JavaScript']
 
 two_student = Student('Mary', 'Kuri', 'women')
-two_student.courses_in_progress += ['Java', 'JavaScript']
+two_student.courses_in_progress += ['Java', 'JavaScript', 'Python']
 two_student.finished_courses += ['Python', 'Git']
 
 one_lecturer = Lecturer('Some', 'Buddy')
@@ -79,13 +97,16 @@ one_lecturer.courses_attached += ['Python', 'Git', 'Java', 'JavaScript']
 two_lecturer = Lecturer('Piter', 'Jackson')
 two_lecturer.courses_attached += ['Python', 'Git', 'Java', 'JavaScript']
 
-one_student.rate_lec(one_lecturer, 'Java', 8)
-one_student.rate_lec(one_lecturer, 'JavaScript', 4)
+one_student.rate_lec(two_lecturer, 'Python', 5)
+# one_student.grade_student(one_lecturer, 'Java', 8)
+# one_student.grade_student(one_lecturer, 'JavaScript', 4)
 
 two_student.rate_lec(two_lecturer, 'Python', 10)
 two_student.rate_lec(two_lecturer, 'Git', 7)
 
 one_reviewer = Reviewer('Jay', 'Simpson')
+one_reviewer.courses_attached.append('Python')
+one_reviewer.rate_hw(one_student, 'Python', 8)
 
 two_reviewer = Reviewer('Katy', 'Perry')
 
