ba7d53c384f9dd91987dea33dc34e85982f78a0c
dpohlupnev <dpohlupnev@gmail.com>
2021-03-30 19:41:39 +0300

    Update 30.03

--- main.py
+++ main.py
@@ -25,15 +25,16 @@ class Student:
     def grade_student(self):
         sum_grade = []
         for grade in self.hw_grade.items():
-            sum_grade += grade[1]
-        self.average_rating_stud = str(round(sum(sum_grade) / len(sum_grade)))
+            sum_grade.extend(grade[1])
+        self.average_rating_stud = str(round(sum(sum_grade) / len(sum_grade), 1))
         return self.average_rating_stud
 
 # перезагрузка магического метода lt (less than) для сравнения двух студентов (не работает, т.к. не дописан, непонятно как сразу вывести на экран результат сравнения)
     def __lt__(self, other_stud):
         if isinstance(other_stud, Student):
             if other_stud.average_rating_stud < self.average_rating_stud:
-                self.name = 'Лучший студент!'
+                print(f'Лучший студент! {self.name}')
+                print(f'Худший студент {other_stud.name}')
         else:
             return f"{other_stud.name} Не является студентом!"
 
@@ -56,11 +57,11 @@ class Lecturer(Mentor):
         super().__init__(name, surname)
 
 # метод подсчета средней оценки лектора за лекции, поставленной студентом, считается в этом же классе (не работает)
-    def grade_student(self):
+    def grade_lec(self):
         sum_grade = []
         for grade in self.student_grade.items():
-            sum_grade += grade[1]
-        self.average_rating_lec = str(round(sum(sum_grade) / len(sum_grade)))
+            sum_grade.extend(grade[1])
+        self.average_rating_lec = str(round(sum(sum_grade) / len(sum_grade), 1))
 
         return self.average_rating_lec
 
@@ -106,12 +107,12 @@ class Reviewer(Mentor):
 
 
 one_student = Student('Ruoy', 'Eman', 'man')
-one_student.courses_in_progress += ['Python', 'Git']
-one_student.finished_courses += ['Java', 'JavaScript']
+one_student.courses_in_progress += ['Python', 'Git', 'Java', 'JavaScript']
+one_student.finished_courses += ['Python', 'Git', 'Java', 'JavaScript']
 
 two_student = Student('Mary', 'Kuri', 'women')
-two_student.courses_in_progress += ['Java', 'JavaScript', 'Python']
-two_student.finished_courses += ['Python', 'Git']
+two_student.courses_in_progress += ['Python', 'Git', 'Java', 'JavaScript']
+two_student.finished_courses += ['Python', 'Git', 'Java', 'JavaScript']
 
 one_lecturer = Lecturer('Some', 'Buddy')
 one_lecturer.courses_attached += ['Python', 'Git', 'Java', 'JavaScript']
@@ -119,18 +120,31 @@ one_lecturer.courses_attached += ['Python', 'Git', 'Java', 'JavaScript']
 two_lecturer = Lecturer('Piter', 'Jackson')
 two_lecturer.courses_attached += ['Python', 'Git', 'Java', 'JavaScript']
 
-one_student.rate_lec(two_lecturer, 'Python', 5)
-# one_student.grade_student(one_lecturer, 'Java', 8)
-# one_student.grade_student(one_lecturer, 'JavaScript', 4)
+one_student.rate_lec(one_lecturer, 'Python', 5)
+one_student.rate_lec(one_lecturer, 'Git', 8)
+one_student.rate_lec(two_lecturer, 'Java', 3)
+one_student.rate_lec(two_lecturer, 'JavaScript', 2)
 
-two_student.rate_lec(two_lecturer, 'Python', 10)
-two_student.rate_lec(two_lecturer, 'Git', 7)
+two_student.rate_lec(one_lecturer, 'Python', 10)
+two_student.rate_lec(one_lecturer, 'Git', 9)
+two_student.rate_lec(two_lecturer, 'Java', 4)
+two_student.rate_lec(two_lecturer, 'JavaScript', 5)
 
 one_reviewer = Reviewer('Jay', 'Simpson')
 one_reviewer.courses_attached.append('Python')
 one_reviewer.rate_hw(one_student, 'Python', 8)
+one_reviewer.rate_hw(two_student, 'Git', 4)
 
 two_reviewer = Reviewer('Katy', 'Perry')
+two_reviewer.courses_attached.append('Python')
+two_reviewer.rate_hw(one_student, 'Python', 1)
+two_reviewer.rate_hw(two_student, 'Git', 8)
+
+one_student.grade_student()
+one_student.average_rating_stud
+
+# two_student.grade_student()
+# two_student.average_rating_stud
 
 print(one_student)
 print(two_student)
@@ -138,6 +152,8 @@ print(one_lecturer)
 print(two_lecturer)
 print(one_reviewer)
 print(two_reviewer)
+
+# one_student < two_student
 
 # cool_mentor.rate_hw(best_student, 'Python', 10)
 # cool_mentor.rate_hw(best_student, 'Python', 10)
